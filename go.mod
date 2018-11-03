29274ff525493f6feda2aec314852a5fc036d95a
saibing <saibingdeng@gmail.com>
2018-11-03 10:54:08 +0800

    update third-party module versions

--- go.mod
+++ go.mod
@@ -2,9 +2,11 @@ module github.com/saibing/bingo
 
 require (
 	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.1.1 // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.0
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
@@ -21,7 +23,10 @@ require (
 	github.com/slimsag/godocmd v0.0.0-20161025000126-a1005ad29fe3
 	github.com/sourcegraph/ctxvfs v0.0.0-20180418081416-2b65f1b1ea81
 	github.com/sourcegraph/jsonrpc2 v0.0.0-20180831160525-549eb959f029
-	golang.org/x/net v0.0.0-20181029044818-c44066c5c816
-	golang.org/x/tools v0.0.0-20181030221726-6c7e314b6563
+	github.com/stretchr/testify v1.2.2 // indirect
+	golang.org/x/net v0.0.0-20181102091132-c10e9556a7bc
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	golang.org/x/sys v0.0.0-20181031143558-9b800f95dbbc // indirect
+	golang.org/x/tools v0.0.0-20181102223251-96e9e165b75e
 	gopkg.in/inconshreveable/log15.v2 v2.0.0-20180818164646-67afb5ed74ec
 )
--- langserver/langserver_test.go
+++ langserver/langserver_test.go
@@ -4,17 +4,322 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"flag"
+	"fmt"
+	"github.com/saibing/bingo/langserver/util"
+	"log"
 	"net"
 	"os"
+	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/saibing/bingo/pkg/lsp"
 	"github.com/saibing/bingo/pkg/lspext"
-	"github.com/sourcegraph/ctxvfs"
 	"github.com/sourcegraph/jsonrpc2"
 )
+
+var h *LangHandler
+var conn *jsonrpc2.Conn
+var ctx context.Context
+
+const (
+	rootDir        = "test/pkg"
+	rootImportPath = "github.com/saibing/bingo/langserver/test/pkg"
+
+	basicPkgDir           = "test/pkg/basic"
+	detailedPkgDir        = "test/pkg/detailed"
+	xtestPkgDir           = "test/pkg/xtest"
+	testPkgDir            = "test/pkg/test"
+	subdirectoryPkgDir    = "test/pkg/subdirectory"
+	multiplePkgDir        = "test/pkg/multiple"
+	gorootPkgDir          = "test/pkg/goroot"
+	goprojectPkgDir       = "test/pkg/goproject"
+	gomodulePkgDir        = "test/pkg/gomodule"
+	hoverDocsPkgDir       = "test/pkg/docs"
+	issuePkgDir           = "test/pkg/issue"
+	lookupPkgDir          = "test/pkg/lookup"
+	implementationsPkgDir = "test/pkg/implementations"
+	typealiasPkgDir       = "test/pkg/typealias"
+	completionPkgDir      = "test/pkg/completion"
+	exportedPkgDir 		  = "test/pkg/exported_on_unexported"
+	symbolsPkgDir		  = "test/pkg/symbols"
+	xreferencesPkgDir     = "test/pkg/xreferences"
+	unexpectedPkgDir	  = "test/pkg/unexpected_paths"
+	differentPkgDir       = "test/pkg/different"
+	signatruePkgDir       = "test/pkg/signature"
+)
+
+func basicOutput(suffix string) string {
+	return genOutput(basicPkgDir, suffix)
+}
+
+func detailOutput(suffix string) string {
+	return genOutput(detailedPkgDir, suffix)
+}
+
+func exportedOutput(suffix string) string {
+	return genOutput(exportedPkgDir, suffix)
+}
+
+func xtestOutput(suffix string) string {
+	return genOutput(xtestPkgDir, suffix)
+}
+
+func testOutput(suffix string) string {
+	return genOutput(testPkgDir, suffix)
+}
+
+func subdirectoryOutput(suffix string) string {
+	return genOutput(subdirectoryPkgDir, suffix)
+}
+
+func multipleOutput(suffix string) string {
+	return genOutput(multiplePkgDir, suffix)
+}
+
+func gorootOutput(suffix string) string {
+	return getPlatformPath(runtime.GOROOT() + "/" + suffix)
+}
+
+func signatrueOutput(suffix string) string {
+	return genOutput(signatruePkgDir, suffix)
+}
+
+func gorootOutput2(suffix string) string {
+	return genOutput(gorootPkgDir, suffix)
+}
+
+func unexpectedOutput(suffix string) string {
+	return genOutput(unexpectedPkgDir, suffix)
+}
+
+func xreferences(suffix string) string {
+	return genOutput(xreferencesPkgDir, suffix)
+}
+
+func differentOutput(suffix string) string {
+	return genOutput(differentPkgDir, suffix)
+}
+
+func goprojectOutput(suffix string) string {
+	return genOutput(goprojectPkgDir, suffix)
+}
+
+func gomoduleDepOutput(suffix string) string {
+	depPath := filepath.Join(gopathDir, "pkg/mod/github.com/saibing/dep@v1.0.2")
+	return getPlatformPath(depPath + "/" + suffix)
+}
+
+func gomoduleOutput(suffix string) string {
+	return genOutput(gomodulePkgDir, suffix)
+}
+
+func symbolsOutput(suffix string) string {
+	return genOutput(symbolsPkgDir, suffix)
+}
+
+func lookupOutput(suffix string) string {
+	return genOutput(lookupPkgDir, suffix)
+}
+
+func typealiasOutput(suffix string) string {
+	return genOutput(typealiasPkgDir, suffix)
+}
+
+func implementationsOutput(suffix string) string {
+	return genOutput(implementationsPkgDir, suffix)
+}
+
+func genOutput(pkgDir, suffix string) string {
+	return getPlatformPath(currentWorkDir + pkgDir + "/" + suffix)
+}
+
+func getGOPATH() string {
+	gopath := os.Getenv("GOPATH")
+	if gopath == "" {
+		return filepath.Join(os.Getenv("HOME"), "go")
+	}
+
+	paths := strings.Split(gopath, string(os.PathListSeparator))
+	return paths[0]
+}
+
+
+func getPlatformPath(path string) string {
+	s := filepath.ToSlash(path)
+	if runtime.GOOS == "windows" {
+		return "/" + s
+	}
+
+	return s
+}
+
+var (
+	currentWorkDir = ""
+	gopathDir      = ""
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("------main begin------")
+	flag.Parse()
+
+	dir, err := filepath.Abs(rootDir)
+	if err != nil {
+		log.Fatal("TestMain", err)
+	}
+
+	currentWorkDir, err = os.Getwd()
+	if err != nil {
+		log.Fatal("TestMain", err)
+	}
+	currentWorkDir += "/"
+
+	gopathDir = getGOPATH()
+
+	Init(util.PathToURI(dir))
+
+	defer func() {
+		if conn != nil {
+			if err := conn.Close(); err != nil {
+				log.Fatal("conn.Close", err)
+			}
+		}
+	}()
+
+	exitCode := m.Run()
+
+	fmt.Println("------main end------")
+	os.Exit(exitCode)
+}
+
+func Init(root lsp.DocumentURI) {
+	fmt.Printf("root uri is %s\n", root)
+	cfg := NewDefaultConfig()
+	cfg.FuncSnippetEnabled = true
+	cfg.GocodeCompletionEnabled = true
+	cfg.UseBinaryPkgCache = false
+
+	h = &LangHandler{
+		DefaultConfig: cfg,
+		HandlerShared: &HandlerShared{},
+	}
+
+	addr, done := startLanguageServer(jsonrpc2.HandlerWithError(h.handle))
+	defer done()
+	conn = dialLanguageServer(addr)
+	// Prepare the connection.
+	ctx = context.Background()
+	tdCap := lsp.TextDocumentClientCapabilities{}
+	tdCap.Completion.CompletionItemKind.ValueSet = []lsp.CompletionItemKind{lsp.CIKConstant}
+	if err := conn.Call(ctx, "initialize", InitializeParams{
+		InitializeParams: lsp.InitializeParams{
+			RootURI:      root,
+			Capabilities: lsp.ClientCapabilities{TextDocument: tdCap},
+		},
+		NoOSFileSystemAccess: false,
+		RootImportPath:       rootImportPath,
+		BuildContext: &InitializeBuildContextParams{
+			GOOS:     runtime.GOOS,
+			GOARCH:   runtime.GOARCH,
+			GOPATH:   gopathDir,
+			GOROOT:   runtime.GOROOT(),
+			Compiler: runtime.Compiler,
+		},
+	}, nil); err != nil {
+		log.Fatal("conn.Call", err)
+	}
+}
+
+func startLanguageServer(h jsonrpc2.Handler) (addr string, done func()) {
+	bindAddr := ":0"
+	if os.Getenv("CI") != "" || runtime.GOOS == "windows" {
+		// CircleCI has issues with IPv6 (e.g., "dial tcp [::]:39984:
+		// connect: network is unreachable").
+		// Similar error is happens on Windows:
+		// "dial tcp [::]:61898: connectex: The requested address is not valid in its context."
+		bindAddr = "127.0.0.1:0"
+	}
+	l, err := net.Listen("tcp", bindAddr)
+	if err != nil {
+		log.Fatal("net.Listen", err)
+	}
+	go func() {
+		if err := serveServer(context.Background(), l, h); err != nil && !strings.Contains(err.Error(), "use of closed network connection") {
+			log.Fatal("jsonrpc2.Serve:", err)
+		}
+	}()
+	return l.Addr().String(), func() {
+		if err := l.Close(); err != nil {
+			log.Fatal("close listener:", err)
+		}
+	}
+}
+
+func serveServer(ctx context.Context, lis net.Listener, h jsonrpc2.Handler, opt ...jsonrpc2.ConnOpt) error {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			return err
+		}
+		jsonrpc2.NewConn(ctx, jsonrpc2.NewBufferedStream(conn, jsonrpc2.VSCodeObjectCodec{}), h, opt...)
+	}
+}
+
+func dialLanguageServer(addr string, h ...*jsonrpc2.HandlerWithErrorConfigurer) *jsonrpc2.Conn {
+	conn, err := (&net.Dialer{}).Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := jsonrpc2.HandlerWithError(func(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request) (interface{}, error) {
+		// no-op
+		return nil, nil
+	})
+	if len(h) == 1 {
+		handler = h[0]
+	}
+
+	return jsonrpc2.NewConn(
+		context.Background(),
+		jsonrpc2.NewBufferedStream(conn, jsonrpc2.VSCodeObjectCodec{}),
+		handler,
+	)
+}
+
+// tbRun calls (testing.T).Run or (testing.B).Run.
+func tbRun(t testing.TB, name string, f func(testing.TB)) bool {
+	switch tb := t.(type) {
+	case *testing.B:
+		return tb.Run(name, func(b *testing.B) { f(b) })
+	case *testing.T:
+		return tb.Run(name, func(t *testing.T) { f(t) })
+	default:
+		panic(fmt.Sprintf("unexpected %T, want *testing.B or *testing.T", tb))
+	}
+}
+
+func parsePos(s string) (file string, line, char int, err error) {
+	parts := strings.Split(s, ":")
+	if len(parts) != 3 {
+		err = fmt.Errorf("invalid pos %q (%d parts)", s, len(parts))
+		return
+	}
+	file = parts[0]
+	line, err = strconv.Atoi(parts[1])
+	if err != nil {
+		err = fmt.Errorf("invalid line in %q: %s", s, err)
+		return
+	}
+	char, err = strconv.Atoi(parts[2])
+	if err != nil {
+		err = fmt.Errorf("invalid char in %q: %s", s, err)
+		return
+	}
+	return file, line - 1, char - 1, nil // LSP is 0-indexed
+}
 
 
 func startServer(t testing.TB, h jsonrpc2.Handler) (addr string, done func()) {
@@ -151,13 +456,3 @@ func (v *locations) UnmarshalJSON(data []byte) error {
 	return json.Unmarshal(data, &(*v)[0])
 }
 
-
-// mapFS lets us easily instantiate a VFS with a map[string]string
-// (which is less noisy than map[string][]byte in test fixtures).
-func mapFS(m map[string]string) ctxvfs.FileSystem {
-	m2 := make(map[string][]byte, len(m))
-	for k, v := range m {
-		m2[k] = []byte(v)
-	}
-	return ctxvfs.Map(m2)
-}
