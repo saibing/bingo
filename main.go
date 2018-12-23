4f4cd2b2818bae4c4fc583236db58593ae3773f6
saibing <saibingdeng@gmail.com>
2018-12-23 12:04:09 +0800

    add free os memory function

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"runtime/debug"
+	"time"
 
 	"github.com/saibing/bingo/langserver"
 	"github.com/sourcegraph/jsonrpc2"
@@ -22,6 +24,7 @@ var (
 	trace        = flag.Bool("trace", false, "print all requests and responses")
 	logfile      = flag.String("logfile", "", "also log to this file (in addition to stderr)")
 	printVersion = flag.Bool("version", false, "print version and exit")
+	freeosmemory = flag.Int("freeosmemory", 0, "the interval time that aggressively free memory back to the OS, unit is second, default value is 0, means no free memroy back to the OS")
 	pprof        = flag.String("pprof", "", "start a pprof http server (https://golang.org/pkg/net/http/pprof/)")
 
 	// Default Config, can be overridden by InitializationOptions
@@ -47,6 +50,10 @@ func main() {
 		go func() {
 			log.Println(http.ListenAndServe(*pprof, nil))
 		}()
+	}
+
+	if *freeosmemory > 0 {
+		go freeOSMemory()
 	}
 
 	cfg := langserver.NewDefaultConfig()
@@ -135,3 +142,40 @@ func (stdrwc) Close() error {
 	return os.Stdout.Close()
 }
 
+// freeOSMemory should be called in a goroutine, it invokes
+// runtime/debug.FreeOSMemory() more aggressively than the runtime default of
+// 5 minutes after GC.
+//
+// There is a long-standing known issue with Go in which memory is not returned
+// to the OS aggressively enough[1], which coincidently harms our application
+// quite a lot because we perform so many short-burst heap allocations during
+// the type-checking phase.
+//
+// This function should only be invoked in editor mode, not in sourcegraph.com
+// mode, because users running the language server as part of their editor
+// generally expect much lower memory usage. In contrast, on sourcegraph.com we
+// can give our servers plenty of RAM and allow Go to consume as much as it
+// wants. Go does reuse the memory not free'd to the OS, and as such enabling
+// this does _technically_ make our application perform less optimally -- but
+// in practice this has no observable effect in editor mode.
+//
+// The end effect of performing this is that repeating "hover over code" -> "make an edit"
+// 10 times inside a large package like github.com/docker/docker/cmd/dockerd:
+//
+//
+// 	| Real Before | Real After | Real Change | Go Before | Go After | Go Change |
+// 	|-------------|------------|-------------|-----------|----------|-----------|
+// 	| 7.61GB      | 4.12GB     | -45.86%     | 3.92GB    | 3.33GB   | -15.05%   |
+//
+// Where `Real` means real memory reported by OS X Activity Monitor, and `Go`
+// means memory reported by Go as being in use.
+//
+// TL;DR: 46% less memory consumption for users running with the vscode-go extension.
+//
+// [1] https://github.com/golang/go/issues/14735#issuecomment-194470114
+func freeOSMemory() {
+	for {
+		time.Sleep(time.Duration(*freeosmemory) * time.Second)
+		debug.FreeOSMemory()
+	}
+}
