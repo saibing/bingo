680c727cfadfbe60abc70e28327bee76db39fd94
saibing <saibingdeng@gmail.com>
2019-02-24 23:40:37 +0800

    update did change for new file

--- langserver/fs.go
+++ langserver/fs.go
@@ -187,7 +187,7 @@ func (h *overlay) diagnosetics(ctx context.Context, f source.File) {
 
 func bytesOffset(content []byte, pos lsp.Position) int {
 	var line, char, offset int
-	if len(content) == 0 {
+	if len(content) == 0 && pos.Line == 0 && pos.Character == 0 {
 		return 0
 	}
 
